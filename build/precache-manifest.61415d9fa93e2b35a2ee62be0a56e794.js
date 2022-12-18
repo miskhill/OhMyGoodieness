b19cb309661d918de39e83f6edd3280e847ad360
Gary Smith <80961839+miskhill@users.noreply.github.com>
2022-12-18 21:40:20 +0000

    tings

---
+++ build/precache-manifest.61415d9fa93e2b35a2ee62be0a56e794.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa0f1147ef3b170bee4ca9aca9250f2",
+    "url": "/ohmygoodieness/index.html"
+  },
+  {
+    "revision": "c1e1a1da2f032f264705",
+    "url": "/ohmygoodieness/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c3feef9e8d9342b2b2d2",
+    "url": "/ohmygoodieness/static/css/main.f759247e.chunk.css"
+  },
+  {
+    "revision": "c1e1a1da2f032f264705",
+    "url": "/ohmygoodieness/static/js/2.e8fd23a9.chunk.js"
+  },
+  {
+    "revision": "c3feef9e8d9342b2b2d2",
+    "url": "/ohmygoodieness/static/js/main.d01fb1e8.chunk.js"
+  },
+  {
+    "revision": "2fb5c01a8124c6d3dfc6",
+    "url": "/ohmygoodieness/static/js/runtime-main.3a5701ad.js"
+  },
+  {
+    "revision": "bca33830ce160fd98fc6297f83719849",
+    "url": "/ohmygoodieness/static/media/IMG_4349.bca33830.jpg"
+  },
+  {
+    "revision": "7ab1f2bb0448c63e973464815f3fcf8e",
+    "url": "/ohmygoodieness/static/media/IMG_4350.7ab1f2bb.jpg"
+  },
+  {
+    "revision": "daa50e5ad8cf4e732c5eeaa188bdcd44",
+    "url": "/ohmygoodieness/static/media/IMG_4351.daa50e5a.jpg"
+  },
+  {
+    "revision": "b57c33aa62837de80e2edc917c77a9b1",
+    "url": "/ohmygoodieness/static/media/IMG_4352.b57c33aa.jpg"
+  },
+  {
+    "revision": "ba9b0efeec79ed6d4adcf44517e493e1",
+    "url": "/ohmygoodieness/static/media/IMG_4353.ba9b0efe.jpg"
+  },
+  {
+    "revision": "8280955e34feb781b589bd24756266bf",
+    "url": "/ohmygoodieness/static/media/IMG_4354.8280955e.jpg"
+  },
+  {
+    "revision": "7c1d2672479e0ed7174d00207b284f78",
+    "url": "/ohmygoodieness/static/media/background.7c1d2672.jpg"
+  },
+  {
+    "revision": "ac1ced33c8ab3e80368644d2cd66e3cb",
+    "url": "/ohmygoodieness/static/media/background2.ac1ced33.jpg"
+  }
+]);
